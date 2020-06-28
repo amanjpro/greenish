2228f85fce6dbf0fbfc69141575f9e3b3345ffce
amanjpro <amanjpro@gmail.com>
2020-06-28 00:14:17 -0400

    Remove unused method

--- src/main/scala/models/GroupStatus.scala
+++ src/main/scala/models/GroupStatus.scala
@@ -6,9 +6,7 @@ import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 case class GroupStatus(
   group: Group,
   status: Seq[JobStatus],
-) {
-  def countMissing = status.map(_.countMissing).sum
-}
+)
 object GroupStatus {
   implicit val groupStatusDecoder: Decoder[GroupStatus] = deriveDecoder
   implicit val groupStatusEncoder: Encoder[GroupStatus] = deriveEncoder
