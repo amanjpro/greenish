f4c8d91cc50c2cd8303730562efc0ac9d9f67db1
amanjpro <amanjpro@gmail.com>
2020-07-07 01:09:41 -0400

    Add a summary dashboard

--- src/main/resources/dashboard/summary_container.js
+++ src/main/resources/dashboard/summary_container.js
@@ -35,18 +35,37 @@ class SummaryContainer extends React.Component {
   render() {
     const { error, isLoaded, items } = this.state;
     if (error) {
-      return React.createElement('div', null, `Error: ${error.message}`);
+      return (
+        <div>Error: {error.message}</div>
+      )
     } else if (!isLoaded) {
-      return React.createElement('div', null, 'Loading...');
+      return (
+        <div>Loading...</div>
+      )
     } else {
       return (
-        React.createElement('ul', null,
-          items.map(item => (
-            React.createElement('li',
-              {key: `${item.name}`},
-              `${item.name} ${item.status}`)
-          )))
-      );
+        <div key='GridDiv' className='grid-container'>
+          {items.map((group, gid) => (
+            <div key={`group-${gid}`} className='grid-item'>
+              <h3 key={`group-${gid}-header`}>{group.name}</h3>
+              <table key={`group-${gid}-table`}>
+                <tbody key={`group-${gid}-tbody`}>
+                  <tr key={`group-${gid}-tr-header`}>
+                    <th key={`group-${gid}-th-1`}>Job Name</th>
+                    <th key={`group-${gid}-th-2`}># Missing Data Sets</th>
+                  </tr>
+                  {group["status"].map((job, jid) =>(
+                    <tr key={`job-${gid}-${jid}-row`} className={job.alert_level}>
+                      <td key={`job-${gid}-${jid}-name`}>{job.name}</td>
+                      <td key={`job-${gid}-${jid}-missing`}>{job.missing}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ))}
+        </div>
+      )
     }
   }
 }
