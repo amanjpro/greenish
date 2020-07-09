5b3c01e4667b142ffbdba1eaa5d810f44edf3f46
amanjpro <amanjpro@gmail.com>
2020-07-09 12:11:20 -0400

    Fix release script

--- release.sh
+++ release.sh
@@ -1,16 +1,16 @@
 
 NEW_VERSION=$1
-NEXT_SNAPSHOT=$1
+NEXT_SNAPSHOT=$2
 
 git checkout master
 git pull
 
-echo "version in ThisBuild := $NEW_VERSION" > version.sbt
+echo "version in ThisBuild := \"$NEW_VERSION\"" > version.sbt
 git add version.sbt
 git commit -m "Bump version to $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
 
-echo "version in ThisBuild := $NEXT_SNAPSHOT" > version.sbt
+echo "version in ThisBuild := \"$NEXT_SNAPSHOT\"" > version.sbt
 git add version.sbt
 git commit -m "Bump version to $NEXT_SNAPSHOT"
 
