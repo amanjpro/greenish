60b668dbdc1d62e9a76ef3010a54dcf9bdc22a64
amanjpro <amanjpro@gmail.com>
2020-07-10 13:11:20 -0400

    Update version in package.json too, upon release

--- release.sh
+++ release.sh
@@ -6,11 +6,15 @@ git checkout master
 git pull
 
 echo "version in ThisBuild := \"$NEW_VERSION\"" > version.sbt
+jq --arg version "$NEW_VERSION" '. + {"version": $version}' package.json > package-tmp.json
+mv package-tmp.json package.json
 git add version.sbt
 git commit -m "Bump version to $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
 
 echo "version in ThisBuild := \"$NEXT_SNAPSHOT\"" > version.sbt
+jq --arg version "$NEXT_SNAPSHOT" '. + {"version": $version}' package.json > package-tmp.json
+mv package-tmp.json package.json
 git add version.sbt
 git commit -m "Bump version to $NEXT_SNAPSHOT"
 
