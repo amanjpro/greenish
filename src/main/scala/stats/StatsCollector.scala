b2411d8d4e0b8c2b1696a6dfc66db6ca0527371a
amanjpro <amanjpro@gmail.com>
2020-07-14 21:01:11 -0400

    Refactor StatsCollector actor

--- src/main/scala/stats/StatsCollector.scala
+++ src/main/scala/stats/StatsCollector.scala
@@ -1,95 +1,104 @@
 package me.amanj.greenish.stats
 
-import me.amanj.greenish.models._
-import java.time.ZonedDateTime
 import akka.actor.Actor
-import scala.sys.process.Process
-import scala.util.control.NonFatal
 import akka.actor.ActorLogging
-import io.prometheus.client.{Counter, Gauge}
-import akka.http.scaladsl.model.HttpMethod
-import me.amanj.greenish.endpoints.Routes
+import io.prometheus.client.{Counter, Gauge, Histogram, CollectorRegistry}
+import io.prometheus.client.Collector.MetricFamilySamples
+import io.prometheus.client.exporter.common.TextFormat
+import java.util.Enumeration
+import java.io.{StringWriter, Writer}
+import akka.http.scaladsl.model.{MediaType, HttpCharsets, HttpEntity}
+import akka.http.scaladsl.marshalling.{ToEntityMarshaller, Marshaller}
 
-case class IncRequest(method: HttpMethod)
-case class IncBadResponse(method: String)
-case class IncRefresh(job: String)
-case class DecRefresh(job: String)
+class StatsCollector(jobIDs: Set[String],
+    registry: CollectorRegistry = CollectorRegistry.defaultRegistry) extends Actor with ActorLogging {
 
-class StatsCollector(jobIDs: Set[String]) extends Actor with ActorLogging {
-
-  val requestsCounter = Counter.build()
-    .name("greenish_requests_total")
-    .help("Total number of HTTP requests")
-    .labelNames("http_method")
-    .register()
-
-  val badResponsesCounter = Counter.build()
-    .name("greenish_bad_responses_total")
-    .help("Total number of non-OK HTTP reposnoses")
-    .labelNames("http_method")
-    .register()
-
+  // Job related metrics
   val refreshGauge = Gauge.build()
     .name("greenish_active_refresh_tasks")
     .help("Current number active state refresh tasks")
     .labelNames("job_id")
-    .register()
+    .register(registry)
 
-  val responseTime = Counter.build()
-    .name("greenish_response_time_seconds")
-    .help("HTTP Response time")
-    .labelNames("endpoint")
-    .register()
-
-  val refreshTime = Counter.build()
+  val refreshTime = Histogram.build()
     .name("greenish_state_refresh_time_seconds")
     .help("Job state refreshing time")
     .labelNames("job_id")
-    .register()
+    .buckets(StatsCollector.HistogramTimeBuckets:_*)
+    .register(registry)
 
   val refreshCounter = Counter.build()
     .name("greenish_state_refresh_total")
     .help("Total number of job state refresh instances")
     .labelNames("job_id")
-    .register()
+    .register(registry)
 
    val badRefreshCounter = Counter.build()
     .name("greenish_state_refresh_failed_total")
     .help("Total number of failed job state refresh instances")
     .labelNames("job_id")
-    .register()
+    .register(registry)
 
   val missingPeriods = Gauge.build()
     .name("greenish_missing_periods_total")
     .help("Current number of missing dataset periods")
     .labelNames("job_id")
-    .register()
+    .register(registry)
 
   def init(): Unit = {
-    import StatsCollector.AllMethods
-    AllMethods.foreach(requestsCounter.labels(_))
-    AllMethods.foreach(badResponsesCounter.labels(_))
-    jobIDs.foreach(refreshGauge.labels(_))
-    jobIDs.foreach(missingPeriods.labels(_))
-    jobIDs.foreach(refreshTime.labels(_))
+    jobIDs.foreach { jobId =>
+      refreshGauge.labels(jobId)
+      refreshTime.labels(jobId)
+      refreshCounter.labels(jobId)
+      badRefreshCounter.labels(jobId)
+      missingPeriods.labels(jobId)
+    }
   }
   override def receive: Receive = {
-    case IncRequest(method) =>
-      requestsCounter.labels(method.name.toLowerCase).inc()
-      requestsCounter.labels().get
+    case RefreshTime(jobId, time) =>
+      refreshTime.labels(jobId).observe(time)
+    case IncRefresh(jobId) =>
+      refreshCounter.labels(jobId).inc()
+      refreshGauge.labels(jobId).inc()
+    case DecRefresh(jobId) =>
+      refreshGauge.labels(jobId).dec()
+    case IncBadRefresh(jobId) =>
+      badRefreshCounter.labels(jobId).inc()
+    case MissingPeriods(jobId, count) =>
+      missingPeriods.labels(jobId).set(count)
+    case GetPrometheus =>
+      import StatsCollector.{fromRegistry, toPrometheusTextFormat}
+      val metrics = fromRegistry(registry)
+      context.sender ! metrics
   }
 }
 
 object StatsCollector {
-  val AllMethods = Set(
-    "connect",
-    "delete",
-    "get",
-    "head",
-    "options",
-    "patch",
-    "post",
-    "put",
-    "trace",
-    )
+  case class MetricsEntity(samples: Enumeration[MetricFamilySamples])
+
+  private [StatsCollector] val HistogramTimeBuckets =
+    Seq(
+      0.1, 0.3, 0.5, 0.8, 1, 1.3, 1.5, 1.8, 2, 2.5, 3, 3.5, 4, 4.5)
+
+  private[this] val mediaTypeParams = Map("version" -> "0.0.4")
+  private[this] val mediaType = MediaType.customWithFixedCharset(
+    "text", "plain", HttpCharsets.`UTF-8`, params = mediaTypeParams)
+
+  private[stats] def fromRegistry(
+    collectorRegistry: CollectorRegistry): MetricsEntity = {
+    MetricsEntity(collectorRegistry.metricFamilySamples())
+  }
+
+  private[stats] def toPrometheusTextFormat(e: MetricsEntity): String = {
+    val writer: Writer = new StringWriter()
+    TextFormat.write004(writer, e.samples)
+
+    writer.toString
+  }
+
+  implicit val metricsMarshaller: ToEntityMarshaller[MetricsEntity] = {
+    Marshaller.withFixedContentType(mediaType) { s =>
+      HttpEntity(mediaType, toPrometheusTextFormat(s))
+    }
+  }
 }
---
+++ src/main/scala/stats/messages.scala
@@ -0,0 +1,12 @@
+package me.amanj.greenish.stats
+
+import akka.http.scaladsl.model.HttpMethod
+
+sealed trait Message
+case class IncRefresh(job: String) extends Message
+case class DecRefresh(job: String) extends Message
+case class RefreshTime(job: String, duration: Double) extends Message
+case class IncBadRefresh(job: String) extends Message
+case class MissingPeriods(job: String, num: Int) extends Message
+case object GetPrometheus extends Message
+case object GetStats extends Message
