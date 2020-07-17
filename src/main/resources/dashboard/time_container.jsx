58775befd87036495d3686c5e4e3213944f323f5
amanjpro <amanjpro@gmail.com>
2020-07-16 22:27:45 -0400

    Add UTC time to the dashboard

---
+++ src/main/resources/dashboard/time_container.jsx
@@ -0,0 +1,32 @@
+class TimeContainer extends React.Component {
+  intervalID
+  constructor(props) {
+    super(props);
+    this.state = {
+      now: ""
+    };
+  }
+
+  componentDidMount() {
+    this.refresh()
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.intervalID);
+  }
+
+  refresh = () => {
+    const d = new Date()
+    this.setState({now: d.toUTCString()})
+    this.intervalID = setTimeout(this.refresh, fetchInterval);
+  }
+
+  render() {
+    const { now } = this.state;
+    return (
+      <em key={`utc-time-div`}>
+        {now}
+      </em>
+    )
+  }
+}
