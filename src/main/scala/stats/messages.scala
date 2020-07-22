2a887d5e2bc8178e7b54cec9f592a7773b6f1779
amanjpro <amanjpro@gmail.com>
2020-07-21 22:37:52 -0400

    Add metric for oldest missing period in prometheus

--- src/main/scala/stats/messages.scala
+++ src/main/scala/stats/messages.scala
@@ -8,5 +8,6 @@ case class DecRefresh(job: String) extends Message
 case class RefreshTime(job: String, duration: Double) extends Message
 case class IncBadRefresh(job: String) extends Message
 case class MissingPeriods(job: String, num: Int) extends Message
+case class OldestMissingPeriod(job: String, num: Int) extends Message
 case object GetPrometheus extends Message
 case object GetStats extends Message
