5150caad29c30d0f50d294b64f77f3a21c6a42ff
amanjpro <amanjpro@gmail.com>
2020-07-22 21:58:19 -0400

    Add testing JS to Travis

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ const {defaults} = require('jest-config');
 
 module.exports = {
   verbose: true,
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
   transform: {
     "^.+\\.jsx?$": "babel-jest"
   },
